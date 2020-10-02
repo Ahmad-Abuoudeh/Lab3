c5e0b588c391018d1136d049179e0a008c1d193b
Ahmad <lion1000asd@yahoo.com>
2020-10-02 10:27:35 -0400

    Random message

---
+++ src/AddressBook.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> myBuddies;
+    public AddressBook(){
+        myBuddies = new ArrayList<>() ;
+    }
+
+    public void AddBuddy(BuddyInfo aBuddy) {
+        if(aBuddy != null){
+            myBuddies.add(aBuddy) ;
+        }
+    }
+
+    public BuddyInfo RemoveBuddy( int index ){
+        if(index >= 0 && index<myBuddies.size()){
+            return myBuddies.remove(index) ;
+        }
+        return null ;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo( "Tom",  "Carleton",613) ;
+        AddressBook addressBook = new AddressBook();
+        addressBook.AddBuddy(buddy);
+        addressBook.RemoveBuddy(0 ) ;
+    }
+}
+
